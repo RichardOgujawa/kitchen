809ec00b4b0f7d1656b840f617bc2f84e4f3f0e2
Richard Ogujawa <richardogujawa7@gmail.com>
2022-04-06 20:16:06 +0100

    final commit

---
+++ # pseudo-code.md
@@ -0,0 +1,5 @@
+# Pseudo-code 
+
+## Mobile Menu toggle: 
+- Link the display of the mobile menu to the rotation of the cross. If cross, display:flex, if hamburger icon, display:none
+- Then have it automatically go from being a cross to a hamburger icon, as soon it goes to web view. 
--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
+const orange = "#ED553B";
 const prod = document.getElementById("prod");
 const productScroll = document.querySelector(".product");
 const mobileMenu = document.querySelector(".mobileMenu");
+
+const prodUl = document.querySelector(".prodUl");
+const prodBtn = document.querySelector(".prodScroll");
+var scrollOn = true;
 
 /*Menu Scroll Anim*/
 window.addEventListener("scroll", () => {
@@ -27,15 +32,6 @@ window.addEventListener("scroll", () => {
     }
 });
 
-window.addEventListener("resize", ()=> {
-    if (window.innerWidth = 1170) {
-        console.log("resize");
-        mobileMenu.style.display = "none";
-
-        //NOW DO IF THE HAMBURGER ICON IS A CROSS, UNCROSS IT.
-    } else {
-    }
-});
 
 prod.addEventListener("mouseenter", () => {
     // productScroll.style.display = "block";
@@ -64,45 +60,13 @@ productScroll.addEventListener("mouseleave", () => {
     productScroll.style.zIndex = "-99";
 });
 
-/*MENU*/
-const hamburgerCont = document.querySelector("#hamburgerCont");
-const ham1 = document.querySelector(".ham1");
-const ham2 = document.querySelector(".ham2");
-const body1 = document.querySelector(".body");
-const mobileMenuFS = document.querySelector(".mobileMenu");
-var on = true;
-
-hamburgerCont.addEventListener("click", () => {
-    if (on == true) {
-        on = !on;
-        mobileMenuFS.style.display = "flex";
-    } else if (on == false) {
-        on =!on;
-        mobileMenuFS.style.display = "none";
-    }
-    //HAM1
-    ham1.classList.toggle("ham1Act");
-
-    //HAM2
-    ham2.classList.toggle("ham2Act");
-
-    body1.classList.toggle("overflow");
-
-
-
-});
-
-
 
 /*MOBILE MENU SCROLL*/
-const prodUl = document.querySelector(".prodUl");
-const prodBtn = document.querySelector(".prodScroll");
-var scrollOn = true;
 
 prodBtn.addEventListener("click", () => {
 
     if (scrollOn == true) {
-        scrollOn = !scrollOn
+        scrollOn = !scrollOn;
         console.log("clicked");
         prodUl.style.maxHeight = "520px";
         prodUl.style.transition = "0.7s ease";
@@ -114,13 +78,102 @@ prodBtn.addEventListener("click", () => {
 });
 
 
+/*MENU*/
+const hamburgerCont = document.querySelector("#hamburgerCont");
+const ham1 = document.querySelector(".ham1");
+const ham2 = document.querySelector(".ham2");
+const body1 = document.querySelector(".body");
+const mobileMenuFS = document.querySelector(".mobileMenu");
+var on = true;
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 1170) {
+        console.log("web");
+        ham1.style.transform = "rotate(0deg)";
+        ham2.style.transform = "rotate(0deg)";
+        mobileMenu.style.display = "none";
+        prodUl.style.maxHeight = "0px";
+        scrollOn = true;
+        on = true;  
+    } else if (window.innerWidth <= 1170){
+        console.log("mobile");
+    }
+});
+
+hamburgerCont.addEventListener("click", () => {
+if (window.innerWidth <= 1170){
+    if (on == true) {
+        on = !on;
+        console.log("on");
+
+        ham1.style.transform = "rotate(45deg)";
+        ham2.style.transform = "translate(0px, -10px) rotate(-45deg)";
+        body1.style.overflow = "hidden";
+
+    } else if (on == false) {
+        on = !on;
+        console.log("off");
+
+        ham1.style.transform = "rotate(0deg)";
+        ham2.style.transform = "rotate(0deg)";
+        body1.style.overflow = "scroll";
+    }
+
+    if (window.innerWidth <= 1170) {
+
+        if (ham1.style.transform == "rotate(45deg)"){
+                mobileMenu.style.display = "flex";
+            } else if (ham1.style.transform ="rotate(0deg") {
+                mobileMenu.style.display = "none";
+            }
+        }
+    if (window.innerHeight > 1170) {
+        if(ham1.style.transform == "rotate(45deg)"){
+            ham1.style.transform == "rotate(0deg)";
+        } else {
+
+        }
+    }
+}
+
+});
+
 
 /*Cookies*/
 const cookies = document.querySelector(".cookie");
+const cookieBtn = document.getElementById("cookie-btn"); 
 
-cookies.addEventListener("click", () => {
+cookieBtn.addEventListener("click", () => {
     cookies.style.transform = "translate(250%, -100%)";
 })
+
+/*Agency vs. Client View*/
+const agency = document.getElementById("agencyViewBtn"); 
+const client = document.getElementById("clientViewBtn");
+const img1 = document.getElementById("agencyViewImg");
+const img2 = document.getElementById("clientViewImg");
+
+client.addEventListener("click", ()=> {
+    client.style.color = "white";
+    client.style.border = `${orange} 2px solid`
+    client.style.background = `${orange}`;
+    agency.style.color = `${orange}`;
+    agency.style.border = `${orange} 2px solid`
+    agency.style.background = "white";
+    img2.style.display="block";
+    img1.style.display="none";
+});
+
+agency.addEventListener("click", ()=> {
+    agency.style.color = "white";
+    agency.style.border = "black 2px solid"
+    agency.style.background = "black";
+    client.style.color = "black";
+    client.style.border = "black 2px solid"
+    client.style.background = "white";
+    img1.style.display="block";
+    img2.style.display="none";
+});
 
 
 
